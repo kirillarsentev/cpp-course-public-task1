0aaaf5b33a09572517e9072cd73ca705cd979a21
kirillarsentev <arsentiewkirill@yandex.ru>
2023-10-31 19:49:15 +0300

    Task3 has updated

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,15 +1,14 @@
 #pragma once
 #include <iterator>
-template <typename Itr, typename T>
-Itr LowerBound(Itr first, Itr last, const T& value) {
+int* LowerBound(int* first, int* last, int value) {
 	while (first != last) {
-		int* middle = first + std::distance(first, last) / 2;
-		if (*middle < value) {
-			first = middle + 1;
-		}
-		else {
-			last = middle;
-		}
-	}
-	return last;
+int* middle = first + std::distance(first, last) / 2;
+if (*middle < value) {
+first = middle + 1;
+} else {
+last = middle;
 }
+}
+return last;
+}
+ 
